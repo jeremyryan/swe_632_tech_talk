469375215b2d67616b3b27bf114eabf65b5de206
Jeremy Ryan <jeremyryan000@gmail.com>
2018-03-12 11:12:44 -0400

    Updated to use Vue.js

--- js/techtalk.js
+++ js/techtalk.js
@@ -1,87 +1,75 @@
 $(function() {
-    var createMenuSelectToggle = function($nodeSet) {
-        $nodeSet.click(function(e) {
-            e.preventDefault();
-            e.stopPropagation();
-            var $this = $(this);
-            if ($this.is('.pure-menu-disabled')) return;
-            $nodeSet.removeClass('pure-menu-selected');
-            $this.addClass('pure-menu-selected');
-        });
 
-    };
-
-    createMenuSelectToggle($('#grids .pure-menu-item'));
-    $('#grids .pure-menu-link').click(function() {
-        var $link = $(this);
-        console.log($link.attr('id'));
-        switch ($link.attr('id')) {
-            case 'grid-5ths-tab':
-                $('#grid-24ths, #grid-responsive').hide();
-                $('#grid-5ths').show();
-                break;
-            case 'grid-24ths-tab':
-                $('#grid-5ths, #grid-responsive').hide();
-                $('#grid-24ths').show();
-                break;
-            case 'grid-responsive-tab':
-                $('#grid-5ths, #grid-24ths').hide();
-                $('#grid-responsive').show();
-                break;
+    new Vue({
+        el: '#forms',
+        data: {
+            stacked: false,
+            aligned: false
+        },
+        methods: {
+            tabClick: function(e) {
+                var $tab = $(e.target);
+                var tabId = $tab.attr('id');
+                this.stacked = tabId === 'stacked-form-tab';
+                this.aligned = tabId === 'aligned-form-tab';
+            }
         }
     });
 
-    createMenuSelectToggle($('#forms .pure-menu-item'));
-    $('#forms .pure-menu-link').click(function() {
-        var $link = $(this);
-        switch ($link.attr('id')) {
-            case 'default-form-tab':
-                $('#form-demo').removeClass('pure-form-stacked pure-form-aligned');
-                break;
-            case 'stacked-form-tab':
-                $('#form-demo').addClass('pure-form-stacked').removeClass('pure-form-aligned');
-                break;
-            case 'aligned-form-tab':
-                $('#form-demo').addClass('pure-form-aligned').removeClass('pure-form-stacked');
-                break;
+    new Vue({
+        el: '#grids',
+        data: {
+            grid5ths: true,
+            grid24ths: false,
+            gridResponsive: false
+        },
+        methods: {
+            tabClick: function(e) {
+                var $tab = $(e.target);
+                var tabId = $tab.attr('id');
+                this.grid5ths = tabId === 'grid-5ths-tab';
+                this.grid24ths = tabId === 'grid-24ths-tab';
+                this.gridResponsive = tabId === 'grid-responsive-tab';
+            }
         }
     });
 
-    createMenuSelectToggle($('#buttons .pure-menu-item'));
-    $('#toggle-button-group').click(function() {
-        $('#button-group').toggleClass('pure-button-group');
-    });
-
-    createMenuSelectToggle($('#tables .pure-menu-item'));
-    $('#tables .pure-menu-link').click(function() {
-        var $link = $(this);
-        switch ($link.attr('id')) {
-            case 'default-table-tab':
-                $('#tables .pure-table').removeClass('pure-table-bordered pure-table-horizontal pure-table-striped');
-                break;
-            case 'bordered-table-tab':
-                $('#tables .pure-table').addClass('pure-table-bordered').removeClass('pure-table-horizontal pure-table-striped');
-                break;
-            case 'horizontal-table-tab':
-                $('#tables .pure-table').addClass('pure-table-horizontal').removeClass('pure-table-bordered pure-table-striped');
-                break;
-            case 'striped-table-tab':
-                $('#tables .pure-table').addClass('pure-table-striped').removeClass('pure-table-bordered pure-table-horizontal');
-                break;
+    new Vue({
+        el: '#buttons',
+        data: {
+            grouped: false
         }
     });
 
-    createMenuSelectToggle($('#menu-select .pure-menu-item'));
-    createMenuSelectToggle($('#menu .pure-menu-item'));
-    $('#menus .pure-menu-link').click(function() {
-        var $link = $(this);
-        switch ($link.attr('id')) {
-            case 'vertical-menu-tab':
-                $('#menu').removeClass('pure-menu-horizontal');
-                break;
-            case 'horizontal-menu-tab':
-                $('#menu').addClass('pure-menu-horizontal');
-                break;
+    new Vue({
+        el: '#tables',
+        data: {
+            bordered: false,
+            horizontal: false,
+            striped: false
+        },
+        methods: {
+            tabClick: function(e) {
+                var $tab = $(e.target);
+                var tabId = $tab.attr('id');
+                this.bordered = tabId === 'bordered-table-tab';
+                this.horizontal = tabId === 'horizontal-table-tab';
+                this.striped = tabId === 'striped-table-tab';
+            }
+        }
+    });
+
+    new Vue({
+        el: '#menus',
+        data: {
+            horizontal: false
+        },
+        methods: {
+            tabClick: function(e) {
+                var $tab = $(e.target);
+                var tabId = $tab.attr('id');
+                this.horizontal = tabId === 'horizontal-menu-tab';
+            }
         }
     });
 });
